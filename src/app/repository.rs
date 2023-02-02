56ae008110d6768ceb44f0a50579cac7a136cd38
vaniusrb <vanius@gmail.com>
2023-02-01 23:53:12 -0300

    wip: downcast trait

--- src/app/repository.rs
+++ src/app/repository.rs
@@ -1,8 +1,10 @@
+use std::{any::Any, sync::Mutex};
+
 use super::{
     error::AppError,
     pagination::{Limit, Offset},
 };
-use crate::models::product::Product;
+use crate::{models::product::Product, repositories::product::BufferProductRepository};
 use async_trait::async_trait;
 use leptos::Scope;
 use once_cell::sync::OnceCell;
@@ -29,6 +31,8 @@ pub trait Repository: std::fmt::Debug {
     async fn update(&self, cx: Scope, entity: Self::Entity) -> Result<Self::Entity, AppError>;
 
     async fn delete(&self, cx: Scope, id: Uuid) -> Result<Uuid, AppError>;
+
+    fn as_any(&self) -> &dyn Any;
 }
 
 static PRODUCT_REPOSITORY_INSTANCE: OnceCell<
@@ -45,4 +49,25 @@ pub fn set_product_repository(
 
 pub fn product_repository() -> &'static (dyn Repository<Entity = Product> + 'static + Send + Sync) {
     &**PRODUCT_REPOSITORY_INSTANCE.get().unwrap()
+}
+
+static REPOSITORIES: OnceCell<Box<dyn Any + 'static + Send + Sync>> = OnceCell::new();
+
+pub fn add_repository(
+    repository: impl Repository<Entity = Product> + 'static + Send + Sync + std::fmt::Debug,
+) {
+    let repos = REPOSITORIES.lock().unwrap();
+    repos.push(Box::new(repository));
+}
+
+#[test]
+fn test() {
+    let repo: Box<dyn Repository<Entity = _> + 'static + Send + Sync> =
+        Box::new(BufferProductRepository::new());
+
+    let any = (&*repo).as_any();
+
+    if let Some(repo) = any.downcast_ref::<&dyn Repository<Entity = Product>>() {
+        println!("repo!");
+    }
 }
